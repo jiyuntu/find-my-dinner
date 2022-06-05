e9614654ffba40d0c58713c96c3107030ceb6a73
jiyuntu <wendy3925@gmail.com>
2022-06-05 15:49:38 +0800

    remove unnecessary function

--- main.py
+++ main.py
@@ -37,67 +37,11 @@ logger = logging.getLogger(__name__)
 
 async def start(update: Update, context: CallbackContext.DEFAULT_TYPE) -> None:
     """Inform user about what this bot can do"""
-    await update.message.reply_text(
-        "Please select /poll to get a Poll, /quiz to get a Quiz or /preview"
-        " to generate a preview for your poll"
-    )
-
-async def quiz(update: Update, context: CallbackContext.DEFAULT_TYPE) -> None:
-    """Send a predefined poll"""
-    questions = ["1", "2", "4", "20"]
-    message = await update.effective_message.reply_poll(
-        "How many eggs do you need for a cake?", questions, type=Poll.QUIZ, correct_option_id=2
-    )
-    # Save some info about the poll the bot_data for later use in receive_quiz_answer
-    payload = {
-        message.poll.id: {"chat_id": update.effective_chat.id, "message_id": message.message_id}
-    }
-    context.bot_data.update(payload)
-
-
-async def receive_quiz_answer(update: Update, context: CallbackContext.DEFAULT_TYPE) -> None:
-    """Close quiz after three participants took it"""
-    # the bot can receive closed poll updates we don't care about
-    if update.poll.is_closed:
-        return
-    if update.poll.total_voter_count == 3:
-        try:
-            quiz_data = context.bot_data[update.poll.id]
-        # this means this poll answer update is from an old poll, we can't stop it then
-        except KeyError:
-            return
-        await context.bot.stop_poll(quiz_data["chat_id"], quiz_data["message_id"])
-
-
-async def preview(update: Update, context: CallbackContext.DEFAULT_TYPE) -> None:
-    """Ask user to create a poll and display a preview of it"""
-    # using this without a type lets the user chooses what he wants (quiz or poll)
-    button = [[KeyboardButton("Press me!", request_poll=KeyboardButtonPollType())]]
-    message = "Press the button to let the bot generate a preview for your poll"
-    # using one_time_keyboard to hide the keyboard
-    await update.effective_message.reply_text(
-        message, reply_markup=ReplyKeyboardMarkup(button, one_time_keyboard=True)
-    )
-
-# preview
-async def receive_poll(update: Update, context: CallbackContext.DEFAULT_TYPE) -> None:
-    """On receiving polls, reply to it by a closed poll copying the received poll"""
-    actual_poll = update.effective_message.poll
-    # Only need to set the question and options, since all other parameters don't matter for
-    # a closed poll
-    await update.effective_message.reply_poll(
-        question=actual_poll.question,
-        options=[o.text for o in actual_poll.options],
-        # with is_closed true, the poll/quiz is immediately closed
-        is_closed=True,
-        reply_markup=ReplyKeyboardRemove(),
-    )
-
+    await update.message.reply_text("Please select /poll to get a Poll")
 
 async def help_handler(update: Update, context: CallbackContext.DEFAULT_TYPE) -> None:
     """Display a help message"""
-    await update.message.reply_text("Use /quiz, /poll or /preview to test this bot.")
-
+    await update.message.reply_text("Use /poll to test this bot.")
 
 def main() -> None:
     """Run bot."""
@@ -105,12 +49,8 @@ def main() -> None:
     application = Application.builder().token("5483940215:AAFwJxFnCegf2ICWfrQl19Drnkd4_xK4g7E").build()
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("poll", poll))
-    application.add_handler(CommandHandler("quiz", quiz))
-    application.add_handler(CommandHandler("preview", preview))
     application.add_handler(CommandHandler("help", help_handler))
-    application.add_handler(MessageHandler(filters.POLL, receive_poll))
     application.add_handler(PollAnswerHandler(receive_poll_answer))
-    application.add_handler(PollHandler(receive_quiz_answer))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling()
