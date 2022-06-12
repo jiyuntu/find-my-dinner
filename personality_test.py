874e6cb133229f478361302644ec01504cae5791
Avery Hsu <b08902145@csie.ntu.edu.tw>
2022-06-12 16:35:58 +0800

    add: horoscope

--- personality_test.py
+++ personality_test.py
@@ -15,7 +15,7 @@ from user import User
 
 # current_num_questions = {}
 users = {}
-MAX_QUESTIONS = 5
+MAX_QUESTIONS = 1
 
 async def poll(update: Update, context: ContextTypes.DEFAULT_TYPE, chat_id=None) -> None:
     """Sends a predefined poll"""
@@ -23,25 +23,45 @@ async def poll(update: Update, context: ContextTypes.DEFAULT_TYPE, chat_id=None)
     chat_id = update.effective_chat.id if update else chat_id
     if chat_id not in users:
         users[chat_id] = User(qdb.qnum, chat_id)
-    # obtain question id
-    question_id = users[chat_id].qid_sequence[users[chat_id].qcount]
-    users[chat_id].qcount += 1
-    # construct message
-    message = await context.bot.send_poll(
-        chat_id,
-        qdb.question_list[question_id],
-        qdb.options_list[question_id],
-        is_anonymous=False,
-    )
-    # Save some info about the poll the bot_data for later use in receive_poll_answer
-    payload = {
-        message.poll.id: {
-            "options": qdb.options_list[question_id],
-            "scores" : qdb.scores_list[question_id],
-            "message_id": message.message_id,
-            "chat_id": chat_id,
+    
+    # do personality test
+    if users[chat_id].personality < 0:
+        # obtain question id
+        question_id = users[chat_id].qid_sequence[users[chat_id].qcount]
+        users[chat_id].qcount += 1
+        # construct message
+        message = await context.bot.send_poll(
+            chat_id,
+            qdb.question_list[question_id],
+            qdb.options_list[question_id],
+            is_anonymous=False,
+        )
+        # Save some info about the poll the bot_data for later use in receive_poll_answer
+        payload = {
+            message.poll.id: {
+                "options": qdb.options_list[question_id],
+                "scores" : qdb.scores_list[question_id],
+                "message_id": message.message_id,
+                "chat_id": chat_id,
+            }
         }
-    }
+        
+    # ask for the horoscope
+    else:
+        horoscope_list = qdb.horoscope_list1 if users[chat_id].horoscope==-1 else qdb.horoscope_list2
+        message = await context.bot.send_poll(
+            chat_id,
+            "請問您的星座是？",
+            horoscope_list,
+            is_anonymous=False,
+        )
+        payload = {
+            message.poll.id: {
+                "options": horoscope_list,
+                "message_id": message.message_id,
+                "chat_id": chat_id,
+            }
+        }
     context.bot_data.update(payload)
 
 
@@ -59,32 +79,50 @@ async def receive_poll_answer(update: Update, context: ContextTypes.DEFAULT_TYPE
         return
     selected_options = answer.option_ids
     answer_string = options[selected_options[0]]
-    # show the user their response
-    await context.bot.send_message(
-        chat_id,
-        f"{update.effective_user.mention_html()} 回答{answer_string}！",
-        parse_mode=ParseMode.HTML,
-    )
-    # update user's scores
-    scores = answered_poll["scores"][selected_options[0]]
-    users[chat_id].update_scores(scores)
+    # do personality test
+    if users[chat_id].personality < 0:
+        # show the user their response
+        await context.bot.send_message(
+            chat_id,
+            f"{update.effective_user.mention_html()} 回答：{answer_string}。",
+            parse_mode=ParseMode.HTML,
+        )
+        # update user's scores
+        scores = answered_poll["scores"][selected_options[0]]
+        users[chat_id].update_scores(scores)
+    # ask for the horoscope
+    else:
+        await context.bot.send_message(
+            chat_id,
+            f"{update.effective_user.mention_html()} 回答：{answer_string}。",
+            parse_mode=ParseMode.HTML,
+        )
+        user_horoscope = users[chat_id].update_horoscope(answer_string, selected_options[0])
+        print(user_horoscope)
     # show next poll (or stop)
     await context.bot.stop_poll(chat_id, message_id)
     if users[chat_id].qcount < MAX_QUESTIONS:
+        await poll(None, context, chat_id)
+    elif users[chat_id].personality < 0:
+        # end personality test
+        # obtain the calculated personality
+        personality = users[chat_id].classify_personality()
+        # show the user their personality type
+        await context.bot.send_message(
+            chat_id,
+            f"{update.effective_user.mention_html()}，心理測驗結果出爐！\n你是「{users[chat_id].personality_str}」！",
+            parse_mode=ParseMode.HTML,
+        )
+        # ask for the horoscope
+        await poll(None, context, chat_id)
+    elif users[chat_id].horoscope == -2:
+        # ask for the horoscope AGAIN
         await poll(None, context, chat_id)
     else:
         await make_recommendation(update, context, chat_id)
         del users[chat_id]
 
 async def make_recommendation(update: Update, context: ContextTypes.DEFAULT_TYPE, chat_id=None) -> None:
-    # obtain the calculated personality
-    personality = users[chat_id].classify_personality()
-    # show the user their personality type
-    await context.bot.send_message(
-        chat_id,
-        f"{update.effective_user.mention_html()}，你是{users[chat_id].personality_str}！",
-        parse_mode=ParseMode.HTML,
-    )
     # TODO : using the weather and horoscope,
     # determine the best food to recommend for the user's personality type
     food_str = "土" # to replace
--- user.py
+++ user.py
@@ -8,7 +8,20 @@ class User():
         "健康至上的醫生",
         "愛吃炸雞的小孩"
     ]
-
+    horoscope_list =[
+        "摩羯座",
+        "水瓶座",
+        "雙魚座",
+        "牡羊座",
+        "金牛座",
+        "雙子座",
+        "巨蟹座",
+        "獅子座",
+        "處女座",
+        "天秤座",
+        "天蠍座",
+        "射手座",
+    ]
     def __init__(self, qnum, chat_id):
         # id of the chatroom
         self.chat_id = chat_id
@@ -21,6 +34,9 @@ class User():
         # personality : 0 1 2 3 4, init as -1
         self.personality = -1
         self.personality_str = "未知"
+        # horoscope: 0 1 2 ... 11, init as -1, ask again: -2
+        self.horoscope = -1
+        self.horoscope_str = ""
     
     def update_scores(self, scores):
         for i in range(5):
@@ -31,4 +47,12 @@ class User():
         self.personality_str = User.personality_descriptions[self.personality]
         return self.personality
 
-
+    def update_horoscope(self, ans_str:str, opt_id:int)->str:
+        if ans_str=="以上皆非":
+            self.horoscope = -2
+        elif self.horoscope == -2:
+            self.horoscope = opt_id + 6
+        else:
+            self.horoscope = opt_id
+        self.horoscope_str = ans_str
+        return self.horoscope_str
